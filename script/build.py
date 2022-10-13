7077817823e8fa25b3ca0d30ad6da18f343a1581
yuenze3 <yuenze@jd.com>
2022-10-13 22:45:36 +0800

    chore: try build with swiftshader

--- script/build.py
+++ script/build.py
@@ -31,7 +31,9 @@ def main():
     'skia_use_system_icu=false',
     # 'skia_enable_skshaper=true',
     # 'skia_enable_svg=true',
-    'skia_enable_skottie=true'
+    'skia_enable_skottie=true',
+    'skia_use_vulkan=true',
+    'skia_use_egl=true'
   ]
 
   if 'macos' == system:
